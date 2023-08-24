016bdb77fef55ee2c271a7d5023a324ea31283a4
Muntasir Ifaz <muntasir574@gmail.com>
2023-08-24 14:32:09 +0600

    inital commit

---
+++ android/app/src/main/kotlin/com/batterylow/batterylow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.batterylow.batterylow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'views/ui/welcome.dart';
+import 'views/utils/colors.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: MaterialColor(0xFF2C2C6A, colorMap)
+      ),
+      home: const Welcome(),
+    );
+  }
+}
